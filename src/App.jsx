768648250737c1095bcd2be0bdd0b5dcbfcbf659
sofiaprovilska <mythec93@gmail.com>
2026-01-28 11:11:04 +0100

    dodaje App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import TwoColumns from './TwoColumns';
+
+function App() {
+  return (
+    <div className="app">
+      <TwoColumns />
+    </div>
+  );
+}
+
+export default App;
